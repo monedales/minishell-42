9f73376bd631cff0765e6bd563a46b1e48d84292
Letícia Moneda <le.moneda@outlook.com>
2026-02-10 20:17:21 -0300

    🚧 feat: implement logic for setting and updating environment variables

--- src/env/env_set.c
+++ src/env/env_set.c
@@ -3,32 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   env_set.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mona <mona@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: maria-ol <maria-ol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 00:00:00 by mona              #+#    #+#             */
-/*   Updated: 2026/01/21 20:21:33 by mona             ###   ########.fr       */
+/*   Updated: 2026/02/10 20:14:18 by maria-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @brief Atualiza o valor de uma variável existente
+ * @brief Update the value of an existing variable
  * 
  * TODO: Implementar
  * - Dar free no value antigo
  * - Duplicar o novo value
  * 
- * @param node Nó a ser atualizado
- * @param new_value Novo valor
- * @return SUCCESS ou ERROR
+ * @param node Node to be updated
+ * @param new_value New value for existing variable
+ * @return SUCCESS or ERROR
  */
 static int	update_env_node(t_env *node, const char *new_value)
 {
-	// TODO: Implementar
-	(void)node;
-	(void)new_value;
-	return (ERROR);
+	free(node->value);
+	node->value = ft_strdup(new_value);
+	if (!node->value)
+		return (ERROR);
+	return (SUCCESS);
 }
 
 /**
@@ -53,9 +54,25 @@ static int	update_env_node(t_env *node, const char *new_value)
  */
 int	set_env_value(t_env **env, const char *key, const char *value)
 {
-	// TODO: Implementar
-	(void)env;
-	(void)key;
-	(void)value;
-	return (ERROR);
+	t_env	*current;
+	t_env	*node;
+
+	if (!env || !key)
+		return (ERROR);
+	current = *env;
+	while (current)
+	{
+		if (ft_strlen(current->key) == ft_strlen(key)
+			&& ft_strncmp(current->key, key, ft_strlen(key)) == 0)
+		{
+			update_env_node(current, value);
+			return (SUCCESS);
+		}	
+		current = current->next;
+	}
+	node = create_env_node(key, value);
+	if (!node)
+		return (ERROR);
+	add_env_node(env, node);
+	return (SUCCESS);
 }
