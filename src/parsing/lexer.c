8ccc3fd013debdbb47d2d330823fd6ccdea28817
Letícia Moneda <le.moneda@outlook.com>
2026-02-10 23:07:23 -0300

    🚧 feat: implement token creation logic in create_token function

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mona <mona@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: maria-ol <maria-ol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 00:00:00 by mona              #+#    #+#             */
-/*   Updated: 2026/01/21 20:21:33 by mona             ###   ########.fr       */
+/*   Updated: 2026/02/10 23:01:01 by maria-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,20 @@
  */
 static t_token	*create_token(t_token_type type, char *value)
 {
-	// TODO: Implementar
-	(void)type;
-	(void)value;
-	return (NULL);
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = ft_strdup(value);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->next = NULL;
+	return (token);
 }
 
 /**
